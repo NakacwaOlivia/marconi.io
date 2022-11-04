8a48a3461619447c908c6939645a03996faf6daa
Nakacwa Olivia <k.nakacwa.olivia@gmail.com>
2022-11-04 20:02:27 +0300

    [Refactored]

--- marconi/vue.config.js
+++ marconi/vue.config.js
@@ -1,15 +1,4 @@
-const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-	css: {
-		loaderOptions: {
-			sass: {
-				additionalData: bootstrapSassAbstractsImports.join('\n')
-			},
-			scss: {
-				additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
-			}
-		}
-	}
